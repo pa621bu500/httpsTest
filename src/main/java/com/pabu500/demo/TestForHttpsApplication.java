0da132ae23f6f30d374b489ff7d5d305264a7052
pa621bu500 <pa621bu500@gmail.com>
2024-04-01 03:44:47 +0000

    first commit

---
+++ src/main/java/com/pabu500/demo/TestForHttpsApplication.java
@@ -0,0 +1,13 @@
+package com.pabu500.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForHttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestForHttpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pabu500/demo/TestForHttpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pabu500.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestForHttpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
